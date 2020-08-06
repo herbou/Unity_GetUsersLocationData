6978a70521f9c5d382e17b742196719a46e48115
hamza herbou <hamza95herbou@gmail.com>
2020-08-06 21:07:30 +0100

    add scripts

---
+++ Country.cs
@@ -0,0 +1,20 @@
+﻿public class Country
+{
+
+	public string businessName;
+	public string businessWebsite;
+	public string city;
+	public string continent;
+	public string country;
+	public string countryCode;
+	public string ipName;
+	public string ipType;
+	public string isp;
+	public string lat;
+	public string lon;
+	public string org;
+	public string query;
+	public string region;
+	public string status;
+
+}
---
+++ Locating.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Locating : MonoBehaviour
+{
+	[SerializeField] Text textState;
+	[SerializeField] Text textContinent;
+	[SerializeField] Text textCountry;
+	[SerializeField] Text textCity;
+
+	void Start ()
+	{
+		textState.text = "";
+		StartCoroutine ("DetectCountry");
+	}
+
+	IEnumerator DetectCountry ()
+	{
+		UnityWebRequest request = UnityWebRequest.Get ("https://extreme-ip-lookup.com/json");
+		request.chunkedTransfer = false;
+		yield return request.Send ();
+		textState.text = "Locating...";
+
+		if (request.isError) {
+			textState.text = "error : " + request.error;
+		} else {
+			if (request.isDone) {
+				Country res = JsonUtility.FromJson<Country> (request.downloadHandler.text);
+				textState.text = "";
+				textContinent.text = res.continent;
+				textCity.text = res.city;
+				textCountry.text = res.country;
+			}
+		}
+	}
+}
